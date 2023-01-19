def8dd4794abaaa8de51c73f193cc9b02e0082db
Shmel <Shmel77713@yandex.ru>
2023-01-20 01:25:15 +0400

    Update task 4... Bug git... :/

---
+++ task_4.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: "); //Вывод в консоль сообщение
+int number = Convert.ToInt32(Console.ReadLine()); //int.Parse || convert.ToInt32()
+
+for(int i=2;i<number;i+=2)
+{
+    Console.WriteLine(i);
+}
